ddd210ea5482834273aaebfbb511943f0349a3e5
up2109872 <up2109872@myport.ac.uk>
2024-02-08 14:11:58 +0000

    Update databaseConnection.py

--- databaseConnection.py
+++ databaseConnection.py
@@ -1,38 +1,6 @@
 import pyscopg2 
 
-#create connection to database for a session
-
-connectDb = pyscopg2.connect(
-    database = "",
-    host = "",
-    user = "",
-    password = "",
-    port = ""
-    )
-
-#Database - name of db to access
-#Host - refers to db server ip address or url 
-#User - Postgrese SQL user
-#Password - Postgrese users password
-#Port - locolhost port number on SQL server
-
-
-#create cusor used to run quierries
-cursor = connectDb.cursor()
-
-
-#create functions to run quierries on the database
-def getData():
-    return
-
-def insertData():
-    #might need to split Insert Data methods into different table methods
-    #ie: Modules, Lecturers
-    return
-
-def deleteData():
-    return 
-
+#creates a modify data class with connection to the database
 class ModifyData:
     def __init__(self):
         self.connectDb = pyscopg2.connect(
@@ -43,13 +11,17 @@ class ModifyData:
             port = ""
             )
         
+        #cursor variable uses the connection to the db to search and modify data 
         self.cursor = self.connectDb.cursor()
     
+    #method used to get data 
     def getData(self, query):
         return 0
     
+    #method used to insert data (likely to be split into seperate table methods)
     def insertData(self, data):
         return 0
     
+    #removes selected data
     def deleteData(self, data):
         return 0
