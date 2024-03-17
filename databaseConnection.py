19df68f77c328ff32fccc41b9d368e2afcdded04
up2109872 <up2109872@myport.ac.uk>
2024-03-17 00:37:27 +0000

    updates

--- databaseConnection.py
+++ databaseConnection.py
@@ -4,6 +4,8 @@
 #libary imports 
 import psycopg2
 
+import databaseBuild
+
 
 class db:
 
@@ -21,16 +23,77 @@ class db:
         
         self.dbCursor = self.dbConnection.cursor()
 
-        self.dbExecute = self.dbCursor.execute()
+        self.dbCommit = self.dbConnection.commit()
 
 
         ##Methods 
 
-        def closeConnection(self) -> None:
-            self.dbCusor.close()
+        def closeConnection() -> None:
+            self.dbCursor.close()
             self.dbConnection.close()
             return
-
-
         
+        def buildDatabase(self) -> None:
+            self.dbCursor.execute(
+        
+                """
+                CREATE TABLE IF NOT EXSISTS modules(
+                    mod_id SERIAL PRIMARY KEY,
+                    mod_name VARCHAR(40) NOT NULL, 
+                    mod_enrolled INT NOT NULL,
+                    mod_lectures INT NOT NULL,
+                    mod_practicals INT NOT NULL,
+                    mod_tutorials INT NOT NULL
+                );
+                
+                CREATE TABLE IF NOT EXSISTS lecturer(
+                    lecturer_id SERIAL PRIMARY KEY,
+                    lecturer_fname VARCHAR(20) NOT NULL,
+                    lecturer_lname VARCHAR(20) NOT NUll,
+                    lecturer_availability VARCHAR(100)
+                );
 
+                CREATE TABLE IF NOT EXSISTS building(
+                    building_id SERIAL PRIMMARY KEY,
+                    building_name VARCHAR(20)
+                )
+
+                CREATE TABLE IF NOT EXSISTS room(
+                    room_id SERIAL PRIMARY KEY,
+                    room_name VARCHAR(30),
+                    room_capacity INT NOT NULL,
+                    building_id INT NOT NULL REFERENCES building(building_id)
+                );
+
+                CREATE TABLE IF NOT EXSISTS lecture(
+                    lecuture_id SERIAL PRIMARY KEY,
+                    mod_id INT NOT NULL REFERENCES modules(mod_id),
+                    room_id INT NOT NULL REFERENCES room(room_id),
+                    lecturer_id INT NOT NULL REFERENCES lecturer(lecturer_id),
+                    lecturer_start DECIMAL, 
+                    lecturer_end DECIMAL
+                );
+                """
+            )
+
+            self.dbCommit()
+            return 
+
+def main() -> None:
+
+    session = db()
+
+    session.buildDatabase()
+
+    session.dbCursor.execute(
+        """SELECT * FROM modules"""
+    )
+
+    print(session.dbCursor.fetchall())
+
+    session.closeConnection()
+
+
+#driver 
+if "__name__" == "__main__":
+    main()
