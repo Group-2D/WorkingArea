67584eba237a85fa7f303e212b9a4c8ab54109cb
up2109872 <up2109872@myport.ac.uk>
2024-03-17 00:11:13 +0000

    updates

--- databaseConnection.py
+++ databaseConnection.py
@@ -1,133 +1,36 @@
-import psycopg2 
-import dataValidation
+### File is used to connect the database sever ### 
 
-#creates a modify data class with connection to the database
-class ModifyData:
-    def __init__(self):
-        self.connectDb = psycopg2.connect(
+
+#libary imports 
+import psycopg2
+
+
+class db:
+
+    def __init__ (self) -> None:
+
+        #Attributes
+
+        self.dbConnection = psycopg2.connect(
+            host = "localhost",
             database = "timetable_gen",
-            host = "127.0.0.1",
             user = "postgres",
-            password = "",
-            port = "5432"
-            )
+            password = "Lebihan01!",
+            port = 5432
+         )
         
-        #cursor variable uses the connection to the db to search and modify data 
-        self.cursor = self.connectDb.cursor()
+        self.dbCursor = self.dbConnection.cursor()
 
-    #uses data as an array data[0] == module name
-    #enter into a box -> "Core Computing", 300, 3, 1, 0
-    def insertModuleData(self, data):
-
-        if dataValidation.validateData('Module', data):
-            self.cursor.execute(
-                "INSERT INTO Modules (data) VALUES (%s, %s, %s, %s, %s)",
-                (data[0],data[1], data[2], data[3], data[4])
-            )
-            
-            self.commitChanges()
-            return 
-    
-        else:
-            return False
-
-    #inserts data into the lecturer table
-    def insertLecturerData(self, data):
-        self.cursor.execute(
-            "INSERT INTO Lecturer (data) VALUES (%s, %s, %s,)",
-            (data[0], data[1], data[2])
-        )
-
-        self.commitChanges()
-        return 
+        self.dbExecute = self.dbCursor.execute()
 
 
-    #inserts data into the room table
-    def insertRoomData(self, data):
+        ##Methods 
+
+        def closeConnection(self) -> None:
+            self.dbCusor.close()
+            self.dbConnection.close()
+            return
+
+
         
-        building_id = self.cursor.execute(
-            "SELECT building_id FROM Building WHERE building_name = %s"
-            (data[2])
-        )
 
-        self.cursor.execute(
-            "INSERT INTO Room (data) VALUES (%s, %s, %s)",
-            (data[0], data[1],building_id)
-        )
-
-        self.commitChanges()
-        return 
-    
-
-    #inserts data into the building data 
-    def insertBuildingData(self, building_name):
-        self.cursor.execute(
-            "INSERT INTO Building VALUES (%s)",
-            (building_name)
-        )
-
-        self.commitChanges()
-        return 
-
-    #inserts data into the lecture table
-    def insertLectureData(self, module_name, lecturer_name, room_name):
-        module_id = self.cursor.execute(
-            "SELECT module_id FROM module WHERE module_name = %s"
-            (module_name)
-        )
-    
-        lecturer_id = self.cursor.execute(
-            "SELECT lecturer_id FROM Lecturer WHERE lecturer_name = %s"
-            (lecturer_name)
-        )
-
-        room_id = self.cursor.execute(
-            "SELECT room_id FROM Room WHERE room_name = %s"
-            (room_name)
-        )
-
-        self.cursor.execute(
-            "INSERT INTO Lecture VALUES (%s, %s, %s)"
-            (module_id, lecturer_id, room_id)
-        )
-
-        self.commitChanges()
-        return 
-    
-    #method used to commit changes to the database
-    def commitChanges(self):
-        self.cursor.commit()
-        return 
-    
-    #removes selected data
-    def deleteData(self, table_name, table_info, data):
-        #check the table has data
-        if self.getALLData() == None:
-            return 0
-        #remove data from table
-        else:
-            self.cursor.execute(
-                "DELETE FROM %s WHERE %s = %s "
-                (table_name,table_info, data)
-            )
-
-        self.commitChanges()
-        return 
-    
-    #gets data
-    def getALLData(self, table_name):
-        self.cursor.execute(
-            "SELECT * FROM %s"
-            (table_name.upper())
-        )
-
-        return self.cursor.fetchall()
-    
-    def getData(self, table_name, data):
-        
-        self.cursor.execute(
-            "SELECT * FROM %s WHERE VALUES (%s)"
-            (table_name, data)
-        )
-
-        return self.cursor.fetchone()
--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("hello world")
+import databaseConnection
+
+
+def main():
+    session = databaseConnection
+
+    session.test()
+
+    print(session.cursor.fetchall())
+
+    session.cursor.close()
+
+
+#driver 
+if "__name__" == "__main__":
+    main()
